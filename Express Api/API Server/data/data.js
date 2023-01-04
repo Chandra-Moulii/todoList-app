a18ab2512c5352a60ec46249c8a762091707cf96
Chandra-Moulii <chandramouliv22@gmail.com>
2023-01-04 12:31:29 +0530

    first commit

---
+++ Express Api/API Server/data/data.js
@@ -0,0 +1,34 @@
+let users = [
+  {
+    id: 1,
+    fullName: "Melina Ramos",
+    age: 21,
+  },
+  {
+    id: 2,
+    fullName: "Cheyanne Oneill",
+    age: 18,
+  },
+  {
+    id: 3,
+    fullName: "Saniya Mcconnell",
+    age: 24,
+  },
+  {
+    id: 4,
+    fullName: "Jabari Mcneil",
+    age: 27,
+  },
+  {
+    id: 5,
+    fullName: "Kolby Carson",
+    age: 14,
+  },
+  {
+    id: 6,
+    fullName: "Connor Cannon",
+    age: 5,
+  },
+];
+
+module.exports = users;
